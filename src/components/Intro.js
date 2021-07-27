984b93980cc8481b6b72ebe144ee4ae15eb74031
Jordan Neumann <jsneum01@louisville.edu>
2021-07-27 18:53:17 -0400

    @jsneum01: desktop and mobile styling

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -12,7 +12,7 @@ function Intro () {
       </style>
       <div className="d-flex flex-column align-items-center align-items-lg-start flex-lg-row bg-secondary text-white rounded mx-3 mt-3">
           <img src={logo} className="logo-width p-3"/>
-          <div className="d-flex flex-column align-items-center align-items-lg-start p-3">
+          <div className="d-flex flex-column align-items-center p-3 w-100">
             <h1 className="">AstroJoJo</h1>
             <p style={{"max-width" : "50rem"}}>A web app that consults NASA's Astronomy Picture of the Day API to print out a picture given the date. Made in React!</p>
           </div>
--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,8 +1,8 @@
 function Photo(props) {
     return(
         <div className="d-flex flex-column align-items-center align-items-lg-start flex-lg-row mb-4">
-            <img src={props.photo.url} alt={props.photo.title} className="w-100 rounded mb-3" style={{"max-width" : "40rem"}}/>
-            <div className="d-flex flex-column px-3">
+            <img src={props.photo.url} alt={props.photo.title} className="w-100 rounded mb-3" style={{"max-width" : "45rem"}}/>
+            <div className="d-flex flex-column px-lg-3">
                 <h3 className="h3 bg-dark text-white p-3 rounded mb-3">{props.photo.title}</h3>
                 <p className="bg-dark text-white p-3 mb-0 rounded" style={{"max-width" : "50rem"}}>{props.photo.explanation}</p>
             </div>
